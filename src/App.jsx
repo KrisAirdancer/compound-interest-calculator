979dc9f92334c597814f539b34b5bbacb1503a29
KrisAirdancer <chris.scott.marston@gmail.com>
2023-12-27 19:43:11 -0700

    Cleared up CSS.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App()
 
           <div className="col text-center">
             <i className="bi bi-coin" id='siteIcon'></i>
-            <h1 className='text-center fs-1 text-light' id='siteTitle'>Compound Interest Calculator</h1>
+            <h1 className='text-center fs-1' id='siteTitle'>Compound Interest Calculator</h1>
           </div>
 
         </div>
