576cbada07cb843ff5a57c155d8bcb903158347f
KrisAirdancer <chris.scott.marston@gmail.com>
2023-12-27 21:22:58 -0700

    Added logic to calculate simple future value.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
+import { useState } from 'react'
 import './App.css'
+
+let defaultInvestmentParams = {
+  initialInvestment: 10000,
+  annualAddition: 1200,
+  yearsToGrow: 10,
+  interestRate: 5
+}
 
 function App()
 {
+  const [ investmentParams, setInvestmentParams ] = useState(defaultInvestmentParams)
+
+  const futureValue = deriveFutureValue()
+
+  function handleFieldInput(target, newAmount)
+  {
+    console.log(target)
+
+    // let futureValue = 
+
+    // console.log("future value: " + futureValue)
+
+    // return calculateFutureValue() // TODO: Calculate and update the future value on the UI.
+  }
+
+  function deriveFutureValue()
+  {
+    let newValue = investmentParams.initialInvestment
+
+    for (let i = 0; i < investmentParams.yearsToGrow; i++)
+    {
+      newValue = deriveFutureValueForSinglePeriod(newValue, investmentParams.interestRate)
+    }
+
+    return Math.round(newValue)
+  }
+  
+  function deriveFutureValueForSinglePeriod(p, r)
+  {
+    r = r / 100
+    let n = 1
+    let t = 1
+    return p * ((1 + (r / n)) ** (n * t))
+  }
+
   return (
     <>
       <div className='container' style={{ maxWidth: 768 }}>
@@ -23,24 +66,27 @@ function App()
               <label htmlFor="initialInvestment" className="form-label custom-form-label">Initial Investment</label>
               <div className="input-group">
                 <div className="input-group-text">$</div>
-                <input type="text" className="form-control custom-form-input" id='initialInvestment' defaultValue={10000} />
+                <input onChange={(event) => { handleFieldInput(event.target.id, event.target.value) }} type="text" className="form-control custom-form-input" id='initialInvestment' defaultValue={10000} />
               </div>
 
               <label htmlFor="annualAddition" className="form-label custom-form-label">Annual Addition</label>
               <div className="input-group">
                 <div className="input-group-text">$</div>
-                <input type="text" className="form-control custom-form-input" id='annualAddition' defaultValue={1200} />
+                <input onChange={(event) => { handleFieldInput(event.target.value) }} type="text" className="form-control custom-form-input" id='annualAddition' defaultValue={1200} />
               </div>
 
               <label htmlFor="yearsToGrow" className="form-label custom-form-label">Years to Grow</label>
-                <input type="text" className="form-control custom-form-input" id='yearsToGrow' defaultValue={10} />
+              <input onChange={(event) => { handleFieldInput(event.target.value) }} type="text" className="form-control custom-form-input" id='yearsToGrow' defaultValue={10} />
 
 
               <label htmlFor="interestRate" className="form-label custom-form-label">Interest Rate</label>
               <div className="input-group">
-                <input type="text" className="form-control custom-form-input" id='interestRate' defaultValue={5} />
+                <input onChange={(event) => { handleFieldInput(event.target.value) }} type="text" className="form-control custom-form-input" id='interestRate' defaultValue={5} />
                 <div className="input-group-text">%</div>
               </div>
+
+              <label htmlFor="futureValue" className="form-label custom-form-label">Future Value</label>
+              <input type="text" className="form-control custom-form-input" id='futureValue' value={futureValue} readOnly />
 
             </div>
 
