381ae6ae3bd76a7c9d8cacb7701678719d78ea12
KrisAirdancer <chris.scott.marston@gmail.com>
2023-12-29 16:24:09 -0700

    Added site favicon.

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ export default function Header()
     return (
         <div className="row">
             <div className="col text-center">
-                <i className="bi bi-coin" id='siteIcon'></i>
+                <img className="mt-3" src="../coin.svg" alt="" height={75} />
                 <h1 className='text-center fs-1' id='siteTitle'>Compound Interest Calculator</h1>
           </div>
         </div>
